c20e36ad72b25967f979368c772d6a9049b6d1b7
Paul Keith <javelinanddart@gmail.com>
2015-11-23 10:53:58 -0800

    msm_thermal: Stricter Throttling Policy
    Change-Id: I61297af2738817059860cffc2d0a836ede7dded1
    Signed-off-by: Paul Keith <javelinanddart@gmail.com>

--- drivers/thermal/msm_thermal.c
+++ drivers/thermal/msm_thermal.c
@@ -63,16 +63,16 @@ static struct msm_thermal_tuners {
 } therm_conf = {
 	.start = 0,
 
-	.trip_high_thresh = 76,
-	.reset_high_thresh = 72,
+	.trip_high_thresh = 68,
+	.reset_high_thresh = 64,
 	.freq_high_thresh = 384000,
 
-	.trip_mid_thresh = 70,
-	.reset_mid_thresh = 67,
+	.trip_mid_thresh = 62,
+	.reset_mid_thresh = 59,
 	.freq_mid_thresh = 972000,
 
-	.trip_low_thresh = 64,
-	.reset_low_thresh = 61,
+	.trip_low_thresh = 56,
+	.reset_low_thresh = 53,
 	.freq_low_thresh = 1188000,
 
 	.poll_ms = 3000,
