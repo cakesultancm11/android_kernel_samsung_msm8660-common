4b0e9e8f36bbabb560ee2e02d3947cd6a2a23588
Paul Keith <javelinanddart@gmail.com>
2015-12-27 01:38:04 -0800

    cypress-touchkey: Sweep2Wake Off by Default
    * all teh kool kidz use dt2w
    
    Change-Id: I89125af19a7e71d3fc90ddbb5b4362ffd231db8f
    Signed-off-by: Paul Keith <javelinanddart@gmail.com>

--- drivers/input/keyboard/cypress-touchkey.c
+++ drivers/input/keyboard/cypress-touchkey.c
@@ -152,11 +152,11 @@ static u8 firm_version = 0;
 #endif
 
 #ifdef CONFIG_TOUCH_CYPRESS_SWEEP2WAKE
-int s2w_switch = 1;
-int s2s_switch = 1;
+int s2w_switch = 0;
+int s2s_switch = 0;
 int s2w_start = 0;
 int s2w_count = 0;
-int s2w_lenient = 1;
+int s2w_lenient = 0;
 int dt2w_switch = 2;
 int dt2s_switch = 2;
 int dt2w_start = 0;
